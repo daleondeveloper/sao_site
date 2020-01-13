5b055412df39fbe748ec1154cd3798e3504bce64
Fedunetc_Andriy <daleonandra0206@ukr.net>
2020-01-13 21:51:59 +0200

    First commit

---
+++ src/main/java/com/daleondeveloper/swordartonline_site/SwordartonlineSiteApplication.java
@@ -0,0 +1,13 @@
+package com.daleondeveloper.swordartonline_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordartonlineSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwordartonlineSiteApplication.class, args);
+    }
+
+}
